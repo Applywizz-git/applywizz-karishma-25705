3c07de85b3966aa15732d20d228dc0b398826d1e
Applywizz-git <applywizztechportfolios@gmail.com>
2026-04-16 12:33:29 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { ArrowDown, Download } from "lucide-react";
-import profileImage from "/woman_images.avif";
+import profileImage from "/karishma_portfolio.png";
 
 const techStack = [
   "LLMs",
